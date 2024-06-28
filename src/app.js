50bd8ce0f009695868b728378220d47045f0ad47
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-06-28 20:21:45 +0000

    complete the Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let suit = Math.floor(Math.random() * 4);
+  let number = Math.floor(Math.random() * 12);
+  let color = Math.floor(Math.random() * 2);
+
+  let suitsDivList = document.querySelectorAll(".suit");
+
+  for (let i = 0; i < suitsDivList.length; i++) {
+    suitsDivList[i].innerHTML = suits[suit];
+    color == 1 ? (suitsDivList[i].style.color = "red") : "";
+  }
+
+  let numberDiv = document.querySelector(".number");
+  numberDiv.innerHTML = numbers[number];
+  color == 1 ? (numberDiv.style.color = "red") : "";
 };
